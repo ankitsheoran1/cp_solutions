3638b706aa32b3425a91d067f72c6dcf9dc8fbe2
ankitsheoran1 <ankitsheoran127201@gmail.com>
2019-06-04 00:14:23 +0530

    first commit

---
+++ least_stone_weight.cpp
@@ -0,0 +1,13 @@
+  int lastStoneWeightII(int A[],int n) {
+        boolean[] dp = new boolean[1501];
+        dp[0] = true;
+        int sum = 0;
+        for (int i=0;i<n;i++) {
+            sum += a;
+            for (int j = sum; j >= A[i]; --j)
+                dp[j] |= dp[j - A[i]];
+        }
+        for (int i = sum / 2; i > 0; --i)
+            if (dp[i]) return sum - i - i;
+        return 0;
+    }
